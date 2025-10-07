9a40ff276ef54fe9caff2ccf4b51b6dc7ac29214
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-10-07 15:37:25 +0200

    Try to force the architecture when pulling for the manifest

--- build-manifests.sh
+++ build-manifests.sh
@@ -8,16 +8,15 @@ for manifest in $manifests ; do
   x864Image=$(echo "${manifest}" | sed "s/:[^:]*$/:${branch}-x86_64/")
 
   echo "Pulling ${aarch64Image}"
-  podman pull ${aarch64Image}
+  podman pull --arch arm64 ${aarch64Image}
 
   echo "Pulling ${x864Image}"
-  podman pull ${x864Image}
+  podman pull --arch x86_64 ${x864Image}
 
   if [[ "${branch}" == "main" ]] ; then
     echo "Creating the manifest ${manifest}"
     podman rmi -f ${manifest} || true
     podman manifest create ${manifest} ${aarch64Image} ${x864Image}
-
     podman manifest push --all ${manifest}
   else
     stableManifest=$(echo "${manifest}" | sed "s/:[^:]*$/:${branch}/")
